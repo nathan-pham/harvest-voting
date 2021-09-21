84e510c9bf59bd09d01b6ee0319c100e571bdf12
Nathan Pham <nathanpham.me@gmail.com>
2021-09-21 14:58:58 -0700

    deta key

--- pages/api/vote.js
+++ pages/api/vote.js
@@ -1,6 +1,6 @@
 import { Deta } from "deta"
 
-const deta = Deta(process.env.PROJECT_KEY)
+const deta = Deta(process.env.DETA_PROJECT_KEY)
 const db = deta.Base("votes")
 
 const voteAPI = (req, res) => {
