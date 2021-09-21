7793f3949644294f5f1e60ea830b667faf65f4c1
Nathan Pham <nathanpham.me@gmail.com>
2021-09-21 15:04:36 -0700

    project key again

--- pages/api/vote.js
+++ pages/api/vote.js
@@ -1,11 +1,11 @@
 import { Deta } from "deta"
 
-const deta = Deta(process.env.DETA_PROJECT_KEY)
+const deta = Deta(`${ process.env.DETA_PROJECT_KEY }`)
 const db = deta.Base("votes")
 
-const voteAPI = (req, res) => {
+const voteAPI = async (req, res) => {
     if(req.method == "POST") {
-        db.put(req.body)
+        await db.put(req.body)
         res.status(200).json({ success: true, payload: req.body })
     } else {
         res.status(405).json({ success: false, message: "method not allowed" })
